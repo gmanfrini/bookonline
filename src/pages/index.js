d93132e24e1212b584dcc3ea35fb2431cfe08789
gmanfrini <48986427+gmanfrini@users.noreply.github.com>
2021-03-28 07:54:48 +0200

    Completato json e scaricate immagini

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Home() {
   return (
-  	<div className="bg-gray-500">
-  		prova di testo due
+  	<div className="bg-yellow-500 py-8 px-5 text-red-500">
+  		prova tonino
   	</div>
   )
 }
